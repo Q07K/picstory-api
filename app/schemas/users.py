653fee9d77ced4f99a7dc520447653a634e8734d
gyuhyeong <kgh0730@gmail.com>
2025-07-05 06:36:06 +0900

    Feat: add user schemas for user creation and base information

---
+++ app/schemas/users.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(default=..., description="사용자 이름")
+    email: str = Field(default=None, description="사용자 이메일")
+
+
+class UserCreate(UserBase):
+    password: str = Field(default=..., description="사용자 비밀번호")
+    invitation_code: str = Field(default=..., description="초대 코드")
